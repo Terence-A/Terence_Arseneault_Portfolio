4afcf4e48e39afda28bf4bb2d7fa53f88f74b227
terenceDev <terence.arseneault@hotmail.com>
2024-03-03 16:37:06 -0400

    added dropdown for mobile

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,8 +1,18 @@
 import linkedinImg from "../assets/linkedin.png";
 import githubImg from "../assets/github.png";
 import menuImg from "../assets/icons/menu.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [dropdownActive, setDropdownActive] = useState(false);
+
+  const handleDropdown = () => {
+    console.log("clicked");
+    setDropdownActive(() => !dropdownActive);
+  };
+
+  console.log(dropdownActive);
+
   return (
     <header className="sticky top-0">
       <nav className=" sticky top-0 bg-stone-950 shadow-md shadow-stone-50/10 px-12  py-4 flex justify-between  items-center">
@@ -11,7 +21,6 @@ const Header = () => {
             Terence Arseneault
           </h5>
         </a>
-
         <div className="hidden lg:flex">
           <ul className="flex space-x-12 text-xl uppercase ">
             <a href="#about">
@@ -31,6 +40,7 @@ const Header = () => {
             </a>
           </ul>
         </div>
+        ng
         <div className="hidden lg:flex">
           <ul className="flex space-x-4 xl:w-96 justify-end ">
             <li>
@@ -56,12 +66,35 @@ const Header = () => {
             </a>
           </ul>
         </div>
-        <img
-          className=" flex lg:hidden size-6 "
-          src={menuImg}
-          alt="hamburger menu icon"
-        />
+        <button onClick={() => handleDropdown()}>
+          <img
+            className=" flex lg:hidden size-6 "
+            src={menuImg}
+            alt="hamburger menu icon"
+          />
+        </button>
       </nav>
+      {dropdownActive && (
+        <div className="text-stone-50 py-12 bg-stone-950 lg:hidden">
+          <ul className="flex flex-col items-center space-y-4">
+            <a href="#about" onClick={handleDropdown}>
+              <li className="text-stone-50 hover:text-stone-200 cursor-pointer">
+                About
+              </li>
+            </a>
+            <a href="#portfolio" onClick={handleDropdown}>
+              <li className="text-stone-50 hover:text-stone-200 cursor-pointer">
+                Portfolio
+              </li>
+            </a>
+            <a href="#contact" onClick={handleDropdown}>
+              <li className="text-stone-50 hover:text-stone-200 cursor-pointer">
+                Contact
+              </li>
+            </a>
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
