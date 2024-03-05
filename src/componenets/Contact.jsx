f88315221c30973a9d7c61f529c221d2662545ca
terenceDev <terence.arseneault@hotmail.com>
2024-03-04 20:00:36 -0400

    added form instead of buttons with personal info showing

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -4,8 +4,8 @@ import pinImg from "../assets/icons/pin.png";
 
 const Contact = () => {
   return (
-    <section className="bg-stone-800 py-20 lg:py-48" id="contact">
-      <h1 className="text-3xl lg:text-5xl text-stone-50 text-center mb-12">
+    <section className="bg-stone-800 py-20 lg:py-36" id="contact">
+      {/* <h1 className="text-3xl lg:text-5xl text-stone-50 text-center mb-12">
         Contact Me
       </h1>
       <div className="flex flex-col items-center gap-8 justify-center">
@@ -24,7 +24,35 @@ const Contact = () => {
           <img className="size-8" src={pinImg} alt="email icon" />
           <p className="">Saint John, NB, Canada</p>
         </div>
-      </div>
+      </div> */}
+      <h1 className="text-3xl lg:text-5xl text-stone-50 text-center mb-12">
+        Contact Me
+      </h1>
+      <form className="flex flex-col flex-wrap space-y-4 md:w-[600px] md:mx-auto mx-2">
+        <div className="md:flex-row md:space-x-4 space-y-4 flex-col  ">
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full md:w-[290px] px-4 py-4 bg-stone-950 rounded-sm shadow-md shadow-stone-900 text-xl font-bold placeholder-stone-700 text-stone-50 outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full md:w-[290px] px-4 py-4 bg-stone-950 rounded-sm shadow-md shadow-stone-900 text-xl font-bold placeholder-stone-700 text-stone-50 outline-none"
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Subject"
+          className="w-full px-4 py-4 bg-stone-950 rounded-sm shadow-md shadow-stone-900 text-xl font-bold placeholder-stone-700 text-stone-50 outline-none"
+        />
+        <textarea
+          cols="30"
+          rows="10"
+          placeholder="Message"
+          className="w-full px-4 py-4 bg-stone-950 rounded-sm shadow-md shadow-stone-900 text-xl font-bold placeholder-stone-700 text-stone-50 outline-none"
+        ></textarea>
+      </form>
     </section>
   );
 };
