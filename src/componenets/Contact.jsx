ee7410518e2c4a7864d898d27418b26ebfa4dddd
terenceDev <terence.arseneault@hotmail.com>
2024-03-07 05:39:30 -0400

    added value for form input

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -39,10 +39,11 @@ const Contact = () => {
         onSubmit={handleSubmit}
         className="flex flex-col flex-wrap space-y-4 md:w-[600px] md:mx-auto mx-2"
       >
-        <input type="hidden" name="form-name" className="contactForm" />
+        <input type="hidden" name="form-name" value="contact" />
         <div className="md:flex-row md:space-x-4 space-y-4 flex-col  ">
           <input
             type="text"
+            id="name"
             name="name"
             required
             placeholder="Name"
@@ -50,6 +51,7 @@ const Contact = () => {
           />
           <input
             name="email"
+            id="email"
             type="email"
             placeholder="Email"
             className="w-full md:w-[290px] px-4 py-4 bg-stone-950 rounded-sm shadow-md shadow-stone-900  font-bold placeholder-stone-400 text-stone-50 outline-none"
